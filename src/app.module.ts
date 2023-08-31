7a7f2cd05db0713bb491f3430b17fc4f49f6d557
R3gardless <pidaoh@g.skku.edu>
2023-08-31 14:38:44 +0900

    refactor: Auth Module modules 하위 폴더로 이전

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AppConfigModule } from "./config/app/config.module";
-import { AuthModule } from "./auth/auth.module";
+import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
   imports: [AppConfigModule, AuthModule],
