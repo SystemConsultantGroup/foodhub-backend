5a1835718c5943181e4d1218d95b99fea33bf776
yesjuhee <juhee0924@g.skku.edu>
2023-10-23 11:03:26 +0900

    fix: 테스트 코드 삭제, 업데이트 데이터 전달 방식 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { Oauth2Guard } from "./modules/auth/guards/oauth2.guard";
-import { CurrentUser } from "./common/decorators/current-user.decorator";
 
 @Controller()
 export class AppController {
@@ -10,13 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  // Test API Code
-  @Get("test")
-  @UseGuards(Oauth2Guard({ strict: false, isSignUp: false }))
-  async test(@CurrentUser() user) {
-    console.log(user);
-    return "test success";
   }
 }
